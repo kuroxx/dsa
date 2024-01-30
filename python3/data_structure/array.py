05ea213dc3e3e7c36b936faede75cc63ed8ff864
Anni Chen <annichen13@gmail.com>
2024-01-31 00:09:35 +0800

    add: array data structure

---
+++ python3/data_structure/array.py
@@ -0,0 +1,34 @@
+# list is a collection which is ordered and changeable, with continuous space.
+#
+# Time:  O(1) to index or write or append, O(n) to insert or remove
+# Space: O(n)
+
+def list_operations():
+    # initialization
+    list1 = [3, 1, 2, 4]
+    list2 = list(range(5))
+    list2 = [*range(5)]         # [*X] is equal to list(X)
+
+    # append
+    list1.append(5)
+    list1 += [5]
+    list1 += 5,                 # comma is intentional
+    list1.extend([5, 6])
+
+    # insert
+    list1.insert(0)
+
+    # index
+    list1.index(3)
+
+    # count
+    list1.count(3)
+
+    # remove
+    list1.remove(3)
+
+    # sort
+    list1.sort(reverse=True)
+
+    # reverse
+    list1.reverse()
